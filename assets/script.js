2e76ee6ff588af17e5a7a2b883ed0ff1e534c453
MrHahn <chahn970@gmail.com>
2021-02-02 21:23:08 -0700

    added emailjs

--- assets/script.js
+++ assets/script.js
@@ -19,3 +19,23 @@ XHR.onreadystatechange = function(){
 XHR.open('GET', 'https://api.coindesk.com/v1/bpi/currentprice.json');
 XHR.send();
 }
+
+
+// code fragment
+var data = {
+    service_id: 'bc_notification',
+    template_id: 'template_3vlegl5',
+    user_id: 'user_s5Q5IhQkNKMzjWTuJMyt3'
+};
+ 
+$.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+    type: 'POST',
+    data: JSON.stringify(data),
+    contentType: 'application/json'
+}).done(function() {
+	console.log(data);
+    alert('Your mail is sent!');
+}).fail(function(error) {
+    alert('Oops... ' + JSON.stringify(error));
+});
+// code fragment
